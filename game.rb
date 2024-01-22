f0cde03918673f7b9680722f7df78a425a25b0ce
Jerry Wang <ziyanwang1105@gmail.com>
2024-01-22 13:09:54 -0800

    define game class

--- game.rb
+++ game.rb
@@ -1,1 +1,15 @@
 require_relative './player.rb'
+
+Dictionary = ''
+class Game
+
+
+    def initialize(name1, name2)
+        @fragment = ''
+        @player_1 = Player.new(name1)
+        @player_2 = Player.new(name2)
+        @dictionay = Dictionary
+        @current_player = @player_1
+    end
+
+end
